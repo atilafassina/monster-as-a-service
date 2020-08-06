07095abf8fe981550dc33e877f6e0c86e6ee34c5
Atila Fassina <atila@fassina.eu>
2020-08-06 10:38:03 +0200

    add error handlers

---
+++ README.md
@@ -0,0 +1,16 @@
+# D&D Monster Microservice
+
+This is a microservice wrapping requests to [D&D 5e API](https://www.dnd5eapi.co).
+It will return a random monster at every request.
+
+## Goal
+
+This repository is an example use-case on how to use Netlify Functions with TypeScript.
+
+[Watch the video on Youtube]()
+
+## Deploy your own instance
+
+---
+
+Check the [summary](./SUMMARY.md) if you want to know more.
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,54 @@
 import fetch from 'node-fetch'
 import { Context, APIGatewayEvent } from 'aws-lambda'
 
-type ClassList = {
-  count: number
-  results: any[]
+type MonsterList = {
+  status?: number
+  count?: number
+  results?: any[]
 }
 const DND_5E_API = 'https://www.dnd5eapi.co'
 
-async function getClassData(url) {
-  const resp = await fetch(DND_5E_API + url)
-  return resp.json()
+async function getMonsterData(url) {
+  try {
+    const resp = await fetch(DND_5E_API + '123123' + url)
+    return resp.json()
+  } catch {
+    throw new Error('monster not found')
+  }
 }
 
-async function getClass() {
-  const resp = await fetch(`${DND_5E_API}/api/classes/`)
-  const list: ClassList = await resp.json()
+async function getMonster() {
+  const resp = await fetch(`${DND_5E_API}/api/monsters/`)
+  const list: MonsterList = await resp.json()
+
+  if (!list.count) {
+    throw new Error('no monsters')
+  }
+
   return list.results[Math.floor(Math.random() * list.count)]
 }
 
 export async function handler(event: APIGatewayEvent, context: Context) {
-  const randomClass = await getClass()
-  const data = await getClassData(randomClass.url)
+  try {
+    const randomMonster = await getMonster()
+    const data = await getMonsterData(randomMonster.url)
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      data,
-    }),
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: err.message,
+      }),
+    }
   }
 }
